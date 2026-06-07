ed3732626c1ce0cdf5454e6028ae5f97f1b64aad
Kunal Nimje <kunalnimje00@gmail.com>
2026-06-07 18:44:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public")); // to serve frontend files
+
+app.get("/weather/:city", async (req, res) => {
+    try {
+        const city = req.params.city;
+
+        const response = await fetch(
+            `https://api.weatherapi.com/v1/forecast.json?key=bf3df55bd7eb48d6a58172104260606&q=${city}&days=7&aqi=yes&alerts=yes`
+        );
+
+        const data = await response.json();
+        res.json(data);
+
+    } catch (err) {
+        res.status(500).json({ error: "Something went wrong" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
